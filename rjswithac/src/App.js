4e177f4af54a9104cab9124864f20fa7a1febfdc
uitricks <learn.uitricks@gmail.com>
2022-08-26 22:04:45 +0530

    addReviewWithMutation

--- rjswithac/src/App.js
+++ rjswithac/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import AddReview from './components/AddReview';
 import Locations from './Locations';
 
 
 function App() {
   return (
     <div className="App">
+    <AddReview/>
     <Locations/>
     </div>
   );
--- rjswithac/src/Locations.js
+++ rjswithac/src/Locations.js
@@ -8,6 +8,7 @@ query GetLocations{
     name
     description
     photo
+    overallRating
   }
 }
 `
@@ -16,13 +17,16 @@ export default function Locations() {
     <div>welcome to appollo</div>
     if(loading) return <p>loading...</p>
      if(error) return <p>error</p>
-  return data.locations.map(({id,name,photo,description})=>(
+  return data.locations.map(({id,name,photo,description,overallRating})=>(
     <div key={id}>
+      <h3>{id}</h3>
     <h3>{name}</h3>
     <img width="400" height="250" alt="location-reference" src={photo} />
     <br />
     <b>About this location:</b>
     <p>{description}</p>
+    <b>About this rating:</b>
+    <p>{overallRating}</p>
     <br />
   </div>
   ))
